449c4716d82f41722bea6ac2098141b659d2a700
Fernando <joc.fercho18@gmail.com>
2026-04-25 03:10:37 -0600

    bypass CSRF para pruebas pro

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,8 +10,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        // --- AQUÍ ESTÁ EL CAMBIO ---
+        $middleware->validateCsrfTokens(except: [
+            '*', 
+        ]);
+        // ---------------------------
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
