60421416de6c2291ecadac362ed5333c122cb553
Fernando <joc.fercho18@gmail.com>
2026-04-26 02:53:53 -0600

    Restaurando estabilidad para revisión matutina

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (app()->environment('production')) {
-            URL::forceScheme('http');
+            URL::forceScheme('https');
         }
     }
 }
