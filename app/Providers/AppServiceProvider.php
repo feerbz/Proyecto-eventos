e221905d53cb7151df14719739e15161df821389
Fernando <joc.fercho18@gmail.com>
2026-04-26 02:50:18 -0600

    Cambiando forceScheme a http para evitar bloqueos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (app()->environment('production')) {
-            URL::forceScheme('https');
+            URL::forceScheme('http');
         }
     }
 }
