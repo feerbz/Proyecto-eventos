703f7a3ea6a7e63519ab34b94fc5563147ceee60
Fernanda Baez <feerchbz@icloud.com>
2026-06-15 00:48:54 -0600

    Borrar ruta migraciones

--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,6 @@ use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Artisan;
 
-Route::get('/run-migrations', function () {
-
-    Artisan::call('migrate', [
-        '--force' => true
-    ]);
-
-    return Artisan::output();
-});
-
 Route::get('/schema-check', function () {
 
     return [
